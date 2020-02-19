0e493612c6efe9bfd62d7c8cb2da5ea2b1a35202
kauroopm <kauroopm@sheridancollege.ca>
2020-02-19 18:57:53 -0500

    deliverable-1

--- src/midtermreviewcodeforpartc/User.java
+++ src/midtermreviewcodeforpartc/User.java
@@ -8,7 +8,7 @@ package midtermreviewcodeforpartc;
 /**
  * A class that models a User for Uno Online.
  * The user has a name and a password.
- * @author dancye, 2019
+ * @author kauro, 2019
  */
 public class User 
 {
